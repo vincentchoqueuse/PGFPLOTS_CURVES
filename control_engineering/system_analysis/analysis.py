00916ee24e0b5b3370d9dffbc29e7d57ce2ea13e
Vincent Choqueuse <vincent.choqueuse@gmail.com>
2016-07-13 15:12:29 +0200

    ellipse

--- control_engineering/system_analysis/analysis.py
+++ control_engineering/system_analysis/analysis.py
@@ -1,23 +1,19 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as lg
-
 from scipy.signal import lti
+
+show=0
 
 # create system
 K=1
-m=0.5
+m=0.7
 wn=1
-system=lti([K],[(1/(wn**2)),2*m/wn,1])
 
+# create system
+system=lti([K],[(1/(wn**2)),2*m/wn,1])
 t,s=system.step()
 w, mag, phase = system.bode()
-
-plt.figure()
-plt.plot(t,s)
-plt.figure()
-plt.semilogx(w, mag)
 
 step_data=np.transpose([np.ravel(t),np.ravel(s)])
 np.savetxt("step_response.csv", step_data, delimiter=",")   #export file
@@ -25,4 +21,9 @@ np.savetxt("step_response.csv", step_data, delimiter=",")   #export file
 bode_data=np.transpose([np.ravel(w),np.ravel(mag),np.ravel(phase)])
 np.savetxt("bode_diagram.csv", bode_data, delimiter=",")   #export file
 
-plt.show()
+if show==1:
+    plt.figure()
+    plt.plot(t,s)
+    plt.figure()
+    plt.semilogx(w, mag)
+    plt.show()
